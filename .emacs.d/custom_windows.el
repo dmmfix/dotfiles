b60af2555e313e356e70d7c86e8cde7049897096
Dave Moore <none>
2017-09-09 13:22:56 -0700

    Bringing over linux mods, normalize to unix text
    * Linux profile
    * Merge useful stuff (todo: actually factor this stuff out)

--- .emacs.d/custom_windows.el
+++ .emacs.d/custom_windows.el
@@ -1,7 +1,10 @@
 ;; custom settings
 (defvar *rocktenn-base-directory* "c:/rocktenn/"
   "Base directory of the Research repository on this machine")
+(defvar *surreal-base-directory* "c:/research-surreal/"
+  "Base directory of the Research repository on this machine")
 
-;; By default, go to the research repo
-(cd *rocktenn-base-directory*)
+;; By default, go to the surreal repo
+(cd *surreal-base-directory*)
 (defun dmm/re () (interactive) (cd *rocktenn-base-directory*))
+(defun dmm/su () (interactive) (cd *surreal-base-directory*))
